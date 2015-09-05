f74318ea4678d16b68b80109548cc6da77c77875
Bram Crielaard <bram@crielaard.co.uk>
2015-09-05 15:36:24 +0200

    Start implementing bubble

--- src/main/java/nl/tudelft/model/Bubble.java
+++ src/main/java/nl/tudelft/model/Bubble.java
@@ -1,7 +1,28 @@
 package nl.tudelft.model;
 
 import nl.tudelft.viewControllers.GameController;
+import org.newdawn.slick.GameContainer;
 
 public class Bubble extends GameController {
+    private int x;
+    private int y;
+    private int size;
+    private int dir;
+    private GameContainer gc;
+
+    public Bubble(int x, int y, int size, GameContainer gc) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.gc = gc;
+        this.dir = 1;
+    }
+
+    public void tick() {
+        if (x > 617||x<0) {
+            dir *= -1;
+        }
+    }
+
 
 }
