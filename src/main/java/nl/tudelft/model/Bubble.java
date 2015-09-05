58d12bc4c2b38105eaf0afddc92f976650b7ad08
Bram Crielaard <bram@crielaard.co.uk>
2015-09-05 16:28:36 +0200

    Update Bubble

--- src/main/java/nl/tudelft/model/Bubble.java
+++ src/main/java/nl/tudelft/model/Bubble.java
@@ -1,26 +1,28 @@
 package nl.tudelft.model;
 
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Image;
 
 public class Bubble extends GameObject {
-    private int x;
-    private int y;
-    private int size;
-    private int dir;
     private GameContainer gc;
+    private int counter;
 
-    public Bubble(int x, int y, int size, GameContainer gc) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
+
+
+    public Bubble(Image image, int x, int y, int size, GameContainer gc) {
+        super(image,x,y,size,size,1);
         this.gc = gc;
-        this.dir = 1;
+        this.counter=0;
     }
 
     public void tick() {
-        if (x > 617 || x < 0) {
-            dir *= -1;
+        if (this.x_location > 617 || this.x_location < 0) {
+            this.speed *= -1;
         }
     }
 
+    public void move() {
+        this.y_location=width*speed*Math.abs((int)(Math.sin(counter)));
+        counter++;
+    }
 }
