2ee03eae02b688763a1e8e67cfc4ddf8df35608f
Wouter Smit <pathemeous@gmail.com>
2015-09-07 13:49:51 +0200

    Add lives and score attributes to Player.class
    These attributes are necessary on top of the extended GameObject.

--- src/main/java/nl/tudelft/model/Player.java
+++ src/main/java/nl/tudelft/model/Player.java
@@ -3,10 +3,15 @@ package nl.tudelft.model;
 import org.newdawn.slick.Image;
 
 public class Player extends GameObject {
+	
+	private int score;
+	private int lives;
 
 	
 	public Player(Image image, int x_location, int y_location, int width, int height, int speed) {
 		super(image, x_location, y_location, width, height, speed);
+		this.score = 0;
+		this.lives = 3;
 	}
 	
 	public void tick() {
@@ -52,6 +57,24 @@ public class Player extends GameObject {
 	 */
 	public int getY() {
 		return y_location;
+	}
+	
+	/**
+	 * Get the amount of lives that the player has.
+	 * 
+	 * @return int the amount of lives the player has left.
+	 */
+	public int getLives() {
+		return this.lives;
+	}
+	
+	/**
+	 * Get the score of the player.
+	 * 
+	 * @return int the current score of the player.
+	 */
+	public int getScore() {
+		return this.score;
 	}
 
 }
