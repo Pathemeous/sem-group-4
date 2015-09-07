3da4cab93b693f7a8ff528d33e7afcb60eea03dc
Damian Voorhout <Damianvoorhout@gmail.com>
2015-09-07 22:29:37 +0200

    switched out titlescreen to 1 with buttons, added functionality for all 4 buttons, added quit button

--- src/main/java/nl/tudelft/semgroup4/StartScreen.java
+++ src/main/java/nl/tudelft/semgroup4/StartScreen.java
@@ -12,7 +12,10 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class StartScreen extends BasicGameState {
-	MouseOverArea mouseOverStart;
+	MouseOverArea mouseOverOnePlayer;
+	MouseOverArea mouseOverTwoPlayer;
+	MouseOverArea mouseOverOptions;
+	MouseOverArea mouseOverQuit;
 	ComponentListener listener;
 	Image titleScreen;
 	Input input;
@@ -20,8 +23,11 @@ public class StartScreen extends BasicGameState {
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {	
 		input = container.getInput();
-		mouseOverStart = new MouseOverArea(container, titleScreen, 211, 391, 230, 80);
-		titleScreen = new Image("src/main/resources/img/titleScreen.png");
+		mouseOverOnePlayer = new MouseOverArea(container, titleScreen, 211, 391, 364, 88);
+		mouseOverTwoPlayer = new MouseOverArea(container, titleScreen, 211, 476, 364, 88);
+		mouseOverOptions = new MouseOverArea(container, titleScreen, 211, 573, 364, 88);		
+		mouseOverQuit = new MouseOverArea(container, titleScreen, 211, 672, 364, 88);
+		titleScreen = new Image("src/main/resources/img/titleScreen2.png");
 		
 	}
 
@@ -34,11 +40,30 @@ public class StartScreen extends BasicGameState {
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int ticks) throws SlickException {		
 		if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			if(mouseOverStart.isMouseOver()) {
+			
+			if(mouseOverOnePlayer.isMouseOver()) {
 				game.enterState(1);
 			}
-		}			
+			else if(mouseOverTwoPlayer.isMouseOver()) {
+				System.out.println("Two player game started");
+			}
+			else if(mouseOverOptions.isMouseOver()) {
+				System.out.println("Options menu opened");
+			}
+			else if(mouseOverQuit.isMouseOver()) {
+				container.exit();
+			}
+		}
+		
+		
+		//used for finding button placement 
+		//dimensions for buttons:
+		//1) 211, 380  width = 575 -211 = 364, height = 88
+		//2) 211, 476 ...
+		System.out.println("x = " + input.getMouseX());
+		System.out.println("y = " + input.getMouseY());
 	}
+	
 
 	@Override
 	public int getID() {
