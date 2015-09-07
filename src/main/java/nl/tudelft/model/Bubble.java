e490881af299e4d44511495d6eef6cda8fb02485
Justin van der Krieken <justin.vd.krieken@gmail.com>
2015-09-07 15:12:14 +0200

    update + render are now methods in GameObject Other refactorings

--- src/main/java/nl/tudelft/model/Bubble.java
+++ src/main/java/nl/tudelft/model/Bubble.java
@@ -1,28 +1,33 @@
 package nl.tudelft.model;
 
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 public class Bubble extends GameObject {
     private GameContainer gc;
     private int counter;
+    private int size;
+    private int speed;
 
 
 
-    public Bubble(Image image, int x, int y, int size, GameContainer gc) {
-        super(image,x,y,size,size,1);
-        this.gc = gc;
+    public Bubble(Image image, int x, int y, int size) {
+        super(image,x,y);
+        this.size = size;
         this.counter=0;
     }
 
-    public void tick() {
-        if (this.x_location > 617 || this.x_location < 0) {
-            this.speed *= -1;
-        }
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        super.render(container, g);
     }
 
-    public void move() {
-        this.y_location=width*speed*Math.abs((int)((Math.sin(counter)*100)));
+    @Override
+    public void update(GameContainer container, int delta) {
+        this.locY = getImage().getWidth() * speed * Math.abs((int)((Math.sin(counter)*100)));
         counter++;
     }
+
 }
