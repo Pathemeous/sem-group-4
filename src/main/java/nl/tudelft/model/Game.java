e2a1fe46e9d87d934b28ffd6f8f4b9d7a0ab5d63
Wouter Smit <pathemeous@gmail.com>
2015-09-08 15:06:45 +0200

    Extend the Game.class constructor with iterator
    Adds an iterator that iterates through the levels.

--- src/main/java/nl/tudelft/model/Game.java
+++ src/main/java/nl/tudelft/model/Game.java
@@ -1,6 +1,7 @@
 package nl.tudelft.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
@@ -8,12 +9,27 @@ import org.newdawn.slick.SlickException;
 public class Game implements Updateable {
     
     private ArrayList<Level> levels;
+    private Iterator<Level> levelIt;
+    private ArrayList<Player> players;
     private Level curLevel;
     private int prevLives;
     private int curLives;
     
-    public Game(ArrayList<Level> levels) {
+    /**
+     * Creates a Game with its levels and players.
+     * 
+     * @param levels ArrayList<Level> - List containing all levels that the game consists of.
+     * @param players ArrayList<Player> - List containing all players that take part in this game.
+     * @throws IllegalArgumentException - Iff <code>levels</code> is empty.
+     */
+    public Game(ArrayList<Level> levels, ArrayList<Player> players) throws IllegalArgumentException {
         this.levels = levels;
+        this.players = players;
+        
+        this.levelIt = this.levels.iterator();
+        this.curLevel = this.levelIt.next();
+        
+        
     }
     
     @Override
