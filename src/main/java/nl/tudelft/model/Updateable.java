6b08faa5a12f5c94b8ae8b417190cf487b3389bc
Wouter Smit <pathemeous@gmail.com>
2015-09-08 12:23:42 +0200

    Add update method to Updateable

--- src/main/java/nl/tudelft/model/Updateable.java
+++ src/main/java/nl/tudelft/model/Updateable.java
@@ -1,5 +1,10 @@
 package nl.tudelft.model;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
 /** package **/ interface Updateable {
+    
+    public void update(GameContainer container, int delta) throws SlickException;
 
 }
