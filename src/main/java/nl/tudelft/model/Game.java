4081878e95060ab5ba25347439dfaddb854a27b3
Wouter Smit <pathemeous@gmail.com>
2015-09-08 16:01:55 +0200

    Finish constructor
    Finishes the constructor, making sure all attributes are initialised
    correctly.

--- src/main/java/nl/tudelft/model/Game.java
+++ src/main/java/nl/tudelft/model/Game.java
@@ -12,24 +12,28 @@ public class Game implements Updateable {
     private Iterator<Level> levelIt;
     private ArrayList<Player> players;
     private Level curLevel;
-    private int prevLives;
+    private int prevLives = 0;
     private int curLives;
     
     /**
      * Creates a Game with its levels and players.
+     * Note that the levels and players must both contain at least one object.
      * 
      * @param levels ArrayList<Level> - List containing all levels that the game consists of.
      * @param players ArrayList<Player> - List containing all players that take part in this game.
-     * @throws IllegalArgumentException - Iff <code>levels</code> is empty.
+     * @throws IllegalArgumentException - If <code>levels</code> or <code>players</code> is empty.
      */
     public Game(ArrayList<Level> levels, ArrayList<Player> players) throws IllegalArgumentException {
         this.levels = levels;
         this.players = players;
         
         this.levelIt = this.levels.iterator();
+        
+        if (!this.levelIt.hasNext() || this.players.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.curLevel = this.levelIt.next();
-        
-        
+        this.curLives = this.getPlayerLives();        
     }
     
     @Override
@@ -37,5 +41,34 @@ public class Game implements Updateable {
         // TODO Auto-generated method stub
         
     }
+    
+    /**
+     * Returns the amount of lives that the players have left.
+     * 
+     * When the players run out of lives, the game is over.
+     * 
+     * 
+     * @return int - the total amount of lives left until the game is over.
+     */
+    private int getPlayerLives() {
+        int result = 0;
+        for (Player player : players) {
+            // TODO: add player.getLives() method.
+        }
+    }
+    
+    private Level getNextLevel() {
+        // TODO
+        return new Level();
+    }
+    
+    public void levelCompleted() {
+        // TODO
+    }
+    
+    public void gameOver() {
+        // TODO
+    }
 
+    
 }
