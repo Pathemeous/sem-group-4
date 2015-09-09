abbec2117a696f2a725de21eee7de67e7f45a061
Wouter Smit <pathemeous@gmail.com>
2015-09-09 09:47:52 +0200

    Add JavaDoc to the interface methods
    > documentation

--- src/main/java/nl/tudelft/model/Renderable.java
+++ src/main/java/nl/tudelft/model/Renderable.java
@@ -9,7 +9,21 @@ import org.newdawn.slick.SlickException;
  * specifically, all objects that are rendered in a level.
  */
 /** package **/ interface Renderable {
-    
+
+    /**
+     * Performs all rendering (drawing) for the implementing class.
+     * 
+     * <p>
+     * This method is part of the {@link nl.tudelft.model.Renderable} interface
+     * </p>
+     * 
+     * @param container
+     *            GameContainer - The Slick2D GameContainer that the game runs in.
+     * @param graphics
+     *            Graphics - The Slick2D Graphics object used for rendering.
+     * @throws SlickException
+     *             - If the implementation of this method fails to render.
+     */
     public void render(GameContainer container, Graphics graphics) throws SlickException;
 
 }
--- src/main/java/nl/tudelft/model/Updateable.java
+++ src/main/java/nl/tudelft/model/Updateable.java
@@ -8,7 +8,22 @@ import org.newdawn.slick.SlickException;
  * on every game tick.
  */
 /** package **/ interface Updateable {
-    
+
+    /**
+     * Performs all on-tick updates for the implementing class.
+     * 
+     * <p>
+     * This method is part of the {@link nl.tudelft.model.Updateable} interface.
+     * </p>
+     * 
+     * @param container
+     *            GameContainer - The Slick2D GameContainer that the game runs in.
+     * @param delta
+     *            int - The amount of time in milliseconds that has passed since the previous
+     *            update.
+     * @throws SlickException
+     *             - If the implementation of this method fails to update.
+     */
     public void update(GameContainer container, int delta) throws SlickException;
 
 }
