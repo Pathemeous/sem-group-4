615ad3a63f19e1973e9217c5d6a6613485b975e6
Bram Crielaard <bram@crielaard.co.uk>
2015-09-11 21:00:28 +0200

    Add start of GameTest

---
+++ src/test/java/nl/tudelft/model/GameTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
