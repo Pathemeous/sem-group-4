c42632f838def92511b9e360d282073215cf7e93
Damian Voorhout <Damianvoorhout@gmail.com>
2015-09-11 23:37:22 +0200

    and more checkstyle

--- src/test/java/nl/tudelft/semgroup4/GameObjectTest.java
+++ src/test/java/nl/tudelft/semgroup4/GameObjectTest.java
@@ -1,3 +1,6 @@
+/**
+ *
+ */
 package nl.tudelft.semgroup4;
 
 import java.io.File;
@@ -16,39 +19,54 @@ import nl.tudelft.model.Wall;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+/**
+ * 
+ * @author Damian
+ *
+ */
 public class GameObjectTest extends OpenGLTestCase {
-	
+	/**
+	 * construct gameobjecttest.
+	 */
 	 public GameObjectTest() {
 	        super();
 	    }
-
-	    public static Test suite() {	    	
+	 /**
+	  *
+	  * @return test suite
+	  */
+	    public static Test suite() {
 	        return new TestSuite(GameObjectTest.class);
 	    }
-	    
+
 	    public void setUp() throws LWJGLException, SlickException{
 	        System.setProperty(
 	                "org.lwjgl.librarypath",
-	                new File(new File(System.getProperty("user.dir"), "target/natives"), (LWJGLUtil
+	                new File(new File(System.getProperty("user.dir"),
+	                		"target/natives"), (LWJGLUtil
 	                        .getPlatformName().equals("macosx")) ? "osx" : LWJGLUtil.getPlatformName())
 	                        .getAbsolutePath());
 	        Display.setDisplayMode(new DisplayMode(1, 1));
 	        Display.create();
 	        Resources.init();
 	    }
-	    
+
 	    /**
-	     * tests the getbounds method of the gameObject, checks if the dimensions are correct
+	     * tests the getbounds method of the gameObject,
+	     *  checks if the dimensions are correct.
 	     * @throws SlickException
 	     */
 	    public void testGetBounds() throws SlickException {
 	    	Resources.init();
 	    	Wall wall = new Wall(Resources.vwallImage, 0, 0);
-	    	assertEquals(wall.getHeight(), (int)wall.getBounds().getHeight());
-	    	assertEquals(wall.getWidth(), (int)wall.getBounds().getWidth());
-	    	assertEquals((int)wall.getLocX(), (int)wall.getBounds().getX());
-	    	assertEquals((int)wall.getLocY(), (int)wall.getBounds().getY());	    	
+	    	assertEquals(wall.getHeight(),
+	    			(int) wall.getBounds().getHeight());
+	    	assertEquals(wall.getWidth(),
+	    			(int) wall.getBounds().getWidth());
+	    	assertEquals((int) wall.getLocX(),
+	    			(int) wall.getBounds().getX());
+	    	assertEquals((int) wall.getLocY(),
+	    			(int) wall.getBounds().getY());
 	    }
 	    
 }
