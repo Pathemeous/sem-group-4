95a3d8380ac8177c58154deda451079796a84b53
Bram Crielaard <bram@crielaard.co.uk>
2015-09-11 20:03:56 +0200

    Add LevelFactoryTest

--- src/test/java/nl/tudelft/model/LevelFactoryTest.java
+++ src/test/java/nl/tudelft/model/LevelFactoryTest.java
@@ -32,5 +32,33 @@ public class LevelFactoryTest extends OpenGLTestCase {
 		list = factory.getAllLevels();
 		assertEquals(list.size(), 4);
 	}
+	
+	public void testGetLevel1() {
+		Game mockedGame = mock(Game.class);
+		LevelFactory factory = new LevelFactory(mockedGame);
+		Level lvl = factory.getLevel(1);
+		assertEquals(lvl.getId(), 1);
+	}
+
+	public void testGetLevel2() {
+		Game mockedGame = mock(Game.class);
+		LevelFactory factory = new LevelFactory(mockedGame);
+		Level lvl = factory.getLevel(2);
+		assertEquals(lvl.getId(), 2);
+	}
+	
+	public void testGetLevel3() {
+		Game mockedGame = mock(Game.class);
+		LevelFactory factory = new LevelFactory(mockedGame);
+		Level lvl = factory.getLevel(3);
+		assertEquals(lvl.getId(), 3);
+	}
+	
+	public void testGetLevel4() {
+		Game mockedGame = mock(Game.class);
+		LevelFactory factory = new LevelFactory(mockedGame);
+		Level lvl = factory.getLevel(4);
+		assertEquals(lvl.getId(), 4);
+	}
 
 }
