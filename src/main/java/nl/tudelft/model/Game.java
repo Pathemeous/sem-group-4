7d4b13f306817473526e5a93699e6e4a792cd9ca
Justin van der Krieken <justin.vd.krieken@gmail.com>
2015-09-17 18:34:31 +0200

    initial contract of logger
    With an example in Game.java.
    For now we can all call the Game.logger (should get a better spot soon)
    >feature

--- src/main/java/nl/tudelft/model/Game.java
+++ src/main/java/nl/tudelft/model/Game.java
@@ -1,5 +1,7 @@
 package nl.tudelft.model;
 
+import static nl.tudelft.semgroup4.logger.LogSeverity.VERBOSE;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -9,8 +11,9 @@ import nl.tudelft.semgroup4.Resources;
 import nl.tudelft.semgroup4.collision.CollisionHandler;
 import nl.tudelft.semgroup4.collision.CollisionHelper;
 import nl.tudelft.semgroup4.collision.DefaultCollisionHandler;
+import nl.tudelft.semgroup4.logger.DefaultLogger;
+import nl.tudelft.semgroup4.logger.Logger;
 import nl.tudelft.semgroup4.util.QuadTree;
-
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -18,6 +21,8 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Game implements Renderable, Modifiable {
+
+    public static final Logger logger = new DefaultLogger();
 
     private final int containerWidth;
     private final int containerHeight;
@@ -48,6 +53,7 @@ public class Game implements Renderable, Modifiable {
      */
     public Game(StateBasedGame mainApp, LinkedList<Player> players, int containerWidth,
             int containerHeight) throws IllegalArgumentException {
+        logger.log(VERBOSE, "Game", "constructor called");
         this.mainApp = mainApp;
         this.containerWidth = containerWidth;
         this.containerHeight = containerHeight;
