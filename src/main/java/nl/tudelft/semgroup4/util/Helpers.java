f66587a3b0cc8e7066d00d995e1c55fbec22ad2c
Wouter Smit <pathemeous@gmail.com>
2015-09-18 10:59:41 +0200

    Remove local var and in randInt utility method
    Removes a local variable that is used to store the return value
    and instead returns immediately.
    
    <naming

--- src/main/java/nl/tudelft/semgroup4/util/Helpers.java
+++ src/main/java/nl/tudelft/semgroup4/util/Helpers.java
@@ -15,7 +15,6 @@ public class Helpers {
      */
     public static int randInt(int min, int max) {
         Random rand = new Random();
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-        return randomNum;
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
