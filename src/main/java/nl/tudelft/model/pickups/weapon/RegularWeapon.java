ee85a70a4c204e108cbfee404e1297b1ce4a92c0
Wouter Smit <pathemeous@gmail.com>
2015-09-24 11:01:35 +0200

    Add resourceWrapper injection to Weapon classes
    < interface

--- src/main/java/nl/tudelft/model/pickups/weapon/RegularWeapon.java
+++ src/main/java/nl/tudelft/model/pickups/weapon/RegularWeapon.java
@@ -1,11 +1,11 @@
 package nl.tudelft.model.pickups.weapon;
 
-import nl.tudelft.semgroup4.Resources.Resources;
+import nl.tudelft.semgroup4.Resources.ResourcesWrapper;
 
 public class RegularWeapon extends Weapon {
 
-    public RegularWeapon(float locX, float locY) {
-        super(Resources.pickupWeaponRegular, Resources.weaponImageRegular, 
+    public RegularWeapon(ResourcesWrapper resources, float locX, float locY) {
+        super(resources.getPickupWeaponRegular(), resources.getWeaponImageRegular(), 
                 locX, locY, false, 1);
     }
 
