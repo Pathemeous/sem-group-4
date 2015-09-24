4468a21d40c1ba6f053604012a076cf9289d601c
Wouter Smit <pathemeous@gmail.com>
2015-09-24 11:15:48 +0200

    Update JavaDoc and formatting for Bubbles
    < layout, documentation

--- src/main/java/nl/tudelft/model/bubble/Bubble6.java
+++ src/main/java/nl/tudelft/model/bubble/Bubble6.java
@@ -6,30 +6,38 @@ import nl.tudelft.semgroup4.Resources.ResourcesWrapper;
  * One of the Bubble types. This is a regular bubble of size 6.
  */
 public class Bubble6 extends Bubble {
-    
+
     /**
      * A simplified constructor, which makes the bubble go to the right by default.
-     * @param locX : the starting x-location of the bubble.
-     * @param locY : the starting y-location of the bubble.
+     * 
+     * @param resources
+     *            {@link ResourcesWrapper} - A new resourceWrapper that this class can use.
+     * @param locX
+     *            : the starting x-location of the bubble.
+     * @param locY
+     *            : the starting y-location of the bubble.
      */
     public Bubble6(ResourcesWrapper resources, float locX, float locY) {
         this(resources, locX, locY, true);
     }
-    
+
     /**
-     * The constructor for this class. Generates a bubble of size 6, with a
-     * maxverticalspeed of 10. Adds two bubbles of size 5 to the list of 
-     * next bubbles; those will be spawned when this bubble is hit.
-     * @param locX : the starting x-location of the bubble.
-     * @param locY : the starting y-location of the bubble.
-     * @param goRight : boolean to indicate if the initial direction of the bubble
-     *      is to the right.
+     * The constructor for this class. Generates a bubble of size 6, with a maxverticalspeed of 10.
+     * Adds two bubbles of size 5 to the list of next bubbles; those will be spawned when this
+     * bubble is hit.
+     * 
+     * @param locX
+     *            : the starting x-location of the bubble.
+     * @param locY
+     *            : the starting y-location of the bubble.
+     * @param goRight
+     *            : boolean to indicate if the initial direction of the bubble is to the right.
      */
     public Bubble6(ResourcesWrapper resources, float locX, float locY, boolean goRight) {
         super(resources.getBubbleImage6(), locX, locY, goRight);
-        
+
         setMaxVerticalSpeed(10.0f);
-        
+
         getNext().add(new Bubble5(new ResourcesWrapper(), locX, locY, true));
         getNext().add(new Bubble5(new ResourcesWrapper(), locX, locY, false));
     }
