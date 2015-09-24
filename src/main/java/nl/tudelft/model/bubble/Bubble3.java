ed77320af8b9862d91b7e99ba11452ed0ba1b814
Wouter Smit <pathemeous@gmail.com>
2015-09-24 11:15:48 +0200

    Update JavaDoc and formatting for Bubbles
    < layout, documentation

--- src/main/java/nl/tudelft/model/bubble/Bubble3.java
+++ src/main/java/nl/tudelft/model/bubble/Bubble3.java
@@ -6,30 +6,40 @@ import nl.tudelft.semgroup4.Resources.ResourcesWrapper;
  * One of the Bubble types. This is a regular bubble of size 3.
  */
 public class Bubble3 extends Bubble {
-    
+
     /**
      * A simplified constructor, which makes the bubble go to the right by default.
-     * @param locX : the starting x-location of the bubble.
-     * @param locY : the starting y-location of the bubble.
+     * 
+     * @param resources
+     *            {@link ResourcesWrapper} - A new resourceWrapper that this class can use.
+     * @param locX
+     *            : the starting x-location of the bubble.
+     * @param locY
+     *            : the starting y-location of the bubble.
      */
     public Bubble3(ResourcesWrapper resources, float locX, float locY) {
         this(resources, locX, locY, true);
     }
-    
+
     /**
-     * The constructor for this class. Generates a bubble of size 3, with a
-     * maxverticalspeed of 7. Adds two bubbles of size 2 to the list of 
-     * next bubbles; those will be spawned when this bubble is hit.
-     * @param locX : the starting x-location of the bubble.
-     * @param locY : the starting y-location of the bubble.
-     * @param goRight : boolean to indicate if the initial direction of the bubble
-     *      is to the right.
+     * The constructor for this class. Generates a bubble of size 3, with a maxverticalspeed of 7.
+     * Adds two bubbles of size 2 to the list of next bubbles; those will be spawned when this
+     * bubble is hit.
+     * 
+     * @param resources
+     *            {@link ResourcesWrapper} - A new resourceWrapper that this class can use.
+     * @param locX
+     *            : the starting x-location of the bubble.
+     * @param locY
+     *            : the starting y-location of the bubble.
+     * @param goRight
+     *            : boolean to indicate if the initial direction of the bubble is to the right.
      */
     public Bubble3(ResourcesWrapper resources, float locX, float locY, boolean goRight) {
         super(resources.getBubbleImage3(), locX, locY, goRight);
-        
+
         setMaxVerticalSpeed(7.0f);
-        
+
         getNext().add(new Bubble2(new ResourcesWrapper(), locX, locY, true));
         getNext().add(new Bubble2(new ResourcesWrapper(), locX, locY, false));
     }
