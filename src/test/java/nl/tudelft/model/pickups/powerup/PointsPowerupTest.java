9cc498fa0d9dcb5214b0b5f8991850fd00062c05
Casper Athmer <casperathmer92@gmail.com>
2015-09-25 17:04:51 +0200

    added pointspoweruptest <testing

---
+++ src/test/java/nl/tudelft/model/pickups/powerup/PointsPowerupTest.java
@@ -0,0 +1,44 @@
+package nl.tudelft.model.pickups.powerup;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import nl.tudelft.model.Player;
+import nl.tudelft.semgroup4.resources.ResourcesWrapper;
+
+import org.junit.Test;
+import org.newdawn.slick.Image;
+
+public class PointsPowerupTest {
+    
+    @Test
+    public void testConstructor() {
+        ResourcesWrapper mockedResources = mock(ResourcesWrapper.class);
+        Image mockedImg = mock(Image.class);
+        when(mockedResources.getPickupPowerPoints()).thenReturn(mockedImg);
+        PointsPowerup powerup = new PointsPowerup(mockedResources, 0, 0);
+        
+        assertEquals(mockedResources.getPickupPowerPoints(), powerup.getImage());
+        assertEquals(0.0f, powerup.getLocX(), 0.0f);
+        assertEquals(0.0f, powerup.getLocY(), 0.0f);
+    }
+    
+    @Test
+    public void testActivate1() {
+        ResourcesWrapper mockedResources = mock(ResourcesWrapper.class);
+        PointsPowerup powerup = new PointsPowerup(mockedResources, 0, 0);
+        
+        Player player = new Player(mockedResources, 0, 0, null, true);
+        
+        assertEquals(0, player.getScore());
+        assertFalse(powerup.isActive());
+        
+        powerup.activate(player);
+        
+        assertEquals(100, player.getScore());
+        assertTrue(powerup.isActive());
+        assertTrue(powerup.willBeRemoved());
+    }
+}
