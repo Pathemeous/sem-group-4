411ed0b1a5b3111c92e004478b7e2d535750c5dd
Casper Athmer <casperathmer92@gmail.com>
2015-09-25 16:28:36 +0200

    added remaining bubble tests

---
+++ src/test/java/nl/tudelft/model/bubble/Bubble3Test.java
@@ -0,0 +1,29 @@
+package nl.tudelft.model.bubble;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import nl.tudelft.semgroup4.resources.ResourcesWrapper;
+
+import org.junit.Test;
+import org.newdawn.slick.Image;
+
+public class Bubble3Test {
+    
+    @Test
+    public void testBubble3Constructor() {
+        Image mockedImg = mock(Image.class);
+        ResourcesWrapper mockedResources = mock(ResourcesWrapper.class);
+        when(mockedResources.getBubbleImage3()).thenReturn(mockedImg);
+        
+        Bubble bubble = new Bubble3(mockedResources, 0, 0);
+        
+        assertEquals(7.0f, bubble.getMaxVerticalSpeed(), 0.0f);
+        assertEquals(mockedImg, bubble.getImage());
+        assertEquals(0.0f, bubble.getLocX(), 0.0f);
+        assertEquals(0.0f, bubble.getLocY(), 0.0f);
+        assertEquals(2, bubble.getNext().size());
+        assertTrue(bubble.getNext().get(0) instanceof Bubble2);
+    }
+}
