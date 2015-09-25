66e3db30f2cb5aea717fafa39f75938d981ed407
Wouter Smit <pathemeous@gmail.com>
2015-09-25 15:42:58 +0200

    Add mocked Resources to WallTest
    < testing

--- src/test/java/nl/tudelft/model/WallTest.java
+++ src/test/java/nl/tudelft/model/WallTest.java
@@ -1,31 +1,38 @@
-//package nl.tudelft.model;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import nl.tudelft.semgroup4.resources.Resources;
-//
-//import org.junit.Test;
-//import org.newdawn.slick.Image;
-//import org.newdawn.slick.SlickException;
-//
-///**
-// * Created by justin on 11/09/15.
-// */
-//public class WallTest extends AbstractOpenGLTestCase {
-//
-//    @Test
-//    public void testConstructor() throws SlickException {
-//        Image img = Resources.wallImage.copy();
-//        Wall wall = new Wall(img, 0, 0);
-//        assertEquals(wall.getLocX(), 0.0f, 0.0f);
-//        assertEquals(wall.getLocY(), 0.0f, 0.0f);
-//        assertEquals(wall.getImage(), img);
-//    }
-//
-//    @Test
-//    public void testUpdate() throws SlickException {
-//        Wall wall = new Wall(Resources.vwallImage, 0, 0);
-//        wall.update(null, 0);
-//    }
-//
-//}
+package nl.tudelft.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Created by justin on 11/09/15.
+ */
+public class WallTest extends AbstractOpenGLTestCase {
+    
+    private Image mockedImage; 
+    
+    @Before
+    public void setUp() {
+        mockedImage = Mockito.mock(Image.class);
+    }
+
+    @Test
+    public void testConstructor() throws SlickException {
+        Image img = mockedImage;
+        Wall wall = new Wall(img, 0, 0);
+        assertEquals(wall.getLocX(), 0.0f, 0.0f);
+        assertEquals(wall.getLocY(), 0.0f, 0.0f);
+        assertEquals(wall.getImage(), img);
+    }
+
+    @Test
+    public void testUpdate() throws SlickException {
+        Wall wall = new Wall(mockedImage, 0, 0);
+        wall.update(null, 0);
+    }
+
+}
