963840cb9220e32b23c08a2fabb3a128b0e292e7
Casper Athmer <casperathmer92@gmail.com>
2015-10-06 14:54:14 +0200

    added back button to highscores screen < user-interface

--- src/main/java/nl/tudelft/semgroup4/HighscoresState.java
+++ src/main/java/nl/tudelft/semgroup4/HighscoresState.java
@@ -2,33 +2,55 @@ package nl.tudelft.semgroup4;
 
 import java.awt.Font;
 
+import nl.tudelft.model.Game;
+import nl.tudelft.semgroup4.logger.LogSeverity;
+import nl.tudelft.semgroup4.resources.ResourcesWrapper;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class HighscoresState extends BasicGameState {
     
+    private MouseOverArea backButton;
+    private Input input;
+    private final ResourcesWrapper resources = new ResourcesWrapper();
+    
     @Override
     public void init(GameContainer container, StateBasedGame mainApp) throws SlickException {
         System.out.println("High scores initiated!");
+        backButton = new MouseOverArea(container, resources.getBackText(),
+                container.getWidth() / 10, container.getHeight() / 10 * 9);
+        input = container.getInput();
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame mainApp, Graphics graphics)
             throws SlickException {
-        Font font = new Font("Calibri", Font.BOLD, 36);
-        TrueTypeFont typeFont = new TrueTypeFont(font, true);
+        graphics.drawImage(resources.getBackText(), container.getWidth() / 10.0f,
+                container.getHeight() / 10 * 9);
         
-        typeFont.drawString(container.getWidth() / 2 - 20.0f, 30.0f, "Highscores", Color.yellow);
+        Font font = new Font("Calibri", Font.BOLD, 46);
+        TrueTypeFont typeFont = new TrueTypeFont(font, true);
+        typeFont.drawString(container.getWidth() / 2 - 60.0f, 30.0f, "HIGHSCORES", Color.yellow);
     }
 
     @Override
-    public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
-        
+    public void update(GameContainer container, StateBasedGame game, int ticks) 
+            throws SlickException {
+        if (backButton.isMouseOver()) {
+            if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+                Game.LOGGER.log(LogSeverity.DEBUG, "HighscoresMenu", 
+                        "User goes back to main menu" );
+                game.enterState(0);
+            }
+        }
     }
 
     @Override
