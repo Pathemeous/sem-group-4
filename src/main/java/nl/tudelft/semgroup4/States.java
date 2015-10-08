200ad8c991d7bb999a98129798849020d67a894a
Justin van der Krieken <justin.vd.krieken@gmail.com>
2015-10-08 16:25:57 +0200

    Added constants that can be used for the states
    < naming

---
+++ src/main/java/nl/tudelft/semgroup4/States.java
@@ -0,0 +1,14 @@
+package nl.tudelft.semgroup4;
+
+/**
+ * Created by justin on 08/10/15.
+ */
+public class States {
+
+    public static final int StartScreenState = 0;
+    public static final int GameState = 1;
+    public static final int OptionsState = 2;
+    public static final int HighscoresState = 3;
+    public static final int ShopState = 4;
+    
+}
