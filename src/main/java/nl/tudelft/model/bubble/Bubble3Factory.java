ce8f985b2294a981a18537bc143ba2bca019c124
Casper Athmer <casperathmer92@gmail.com>
2015-10-14 14:18:10 +0200

    Changed concrete bubble factories to implement both methods of createbubble

--- src/main/java/nl/tudelft/model/bubble/Bubble3Factory.java
+++ src/main/java/nl/tudelft/model/bubble/Bubble3Factory.java
@@ -5,12 +5,17 @@ import nl.tudelft.semgroup4.resources.ResourcesWrapper;
 /**
  * Created by justin on 08/10/15.
  */
-public class Bubble3Factory extends BaseBubbleFactory {
+public class Bubble3Factory extends AbstractBubbleFactory {
 
     public Bubble3Factory(ResourcesWrapper resources) {
         super(resources);
     }
 
+    @Override
+    public Bubble createBubble(float locX, float locY) {
+        return new Bubble3(getResources(), locX, locY);
+    }
+    
     @Override
     public Bubble createBubble() {
         return new Bubble3(getResources(), 0, 0);
--- src/main/java/nl/tudelft/model/bubble/Bubble5Factory.java
+++ src/main/java/nl/tudelft/model/bubble/Bubble5Factory.java
@@ -5,12 +5,17 @@ import nl.tudelft.semgroup4.resources.ResourcesWrapper;
 /**
  * Created by justin on 08/10/15.
  */
-public class Bubble5Factory extends BaseBubbleFactory {
+public class Bubble5Factory extends AbstractBubbleFactory {
 
     public Bubble5Factory(ResourcesWrapper resources) {
         super(resources);
     }
 
+    @Override
+    public Bubble createBubble(float locX, float locY) {
+        return new Bubble5(getResources(), locX, locY);
+    }
+    
     @Override
     public Bubble createBubble() {
         return new Bubble5(getResources(), 0, 0);
