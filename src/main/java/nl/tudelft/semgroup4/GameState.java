0281c34c3ea3f37be818a03e12dfcf6b5db0225c
Casper Athmer <casperathmer92@gmail.com>
2015-10-14 14:32:54 +0200

    Changed method name of getter

--- src/main/java/nl/tudelft/semgroup4/GameState.java
+++ src/main/java/nl/tudelft/semgroup4/GameState.java
@@ -133,7 +133,7 @@ public class GameState extends BasicGameState {
         return pauseScreen;
     }
     
-    protected boolean getPauseScreenOpened() {
+    protected boolean isPauseScreenOpened() {
         return pauseScreenOpened;
     }
     
