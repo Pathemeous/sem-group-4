fcd1d21bdc91dd86304bfc6dbfea4b562fe2b55b
Casper Athmer <casperathmer92@gmail.com>
2015-10-14 20:23:37 +0200

    Created RandomPickupFactory class

---
+++ src/main/java/nl/tudelft/model/pickups/RandomPickupFactory.java
@@ -0,0 +1,11 @@
+package nl.tudelft.model.pickups;
+
+public class RandomPickupFactory implements RandomFactory {
+
+    @Override
+    public Pickup createPickup(float locX, float locY) {
+        
+        return null;
+    }
+    
+}
