121d195ad74f9b8da109917a11ffc154e4b48ace
Casper Athmer <casperathmer92@gmail.com>
2015-10-14 18:28:15 +0200

    Adopted the new keys for all images

--- src/main/java/nl/tudelft/semgroup4/resources/ResourcesWrapper.java
+++ src/main/java/nl/tudelft/semgroup4/resources/ResourcesWrapper.java
@@ -102,103 +102,103 @@ public class ResourcesWrapper {
     }
 
     public Image getWallImage() {
-        return images.get("src\\main\\resources\\img\\wall2_h.png");
+        return images.get("img\\wall2_h.png");
     }
 
     public Image getVwallImage() {
-        return images.get("src\\main\\resources\\img\\wall2_v.png");
+        return images.get("img\\wall2_v.png");
     }
     
     public Image getSmallHWallImage() {
-        return images.get("src\\main\\resources\\img\\small_wall_h.png");
+        return images.get("img\\small_wall_h.png");
     }
     
     public Image getSmallVWallImage() {
-        return images.get("src\\main\\resources\\img\\small_wall_v.png");
+        return images.get("img\\small_wall_v.png");
     }
 
     public Image getWeaponImageRegular() {
-        return images.get("src\\main\\resources\\img\\weapon_arrow.png");
+        return images.get("img\\weapon_arrow.png");
     }
 
     public Image getWeaponImageSticky() {
-        return images.get("src\\main\\resources\\img\\weapon_arrow.png");
+        return images.get("img\\weapon_arrow.png");
     }
 
     public Image getWeaponImageFlower() {
-        return images.get("src\\main\\resources\\img\\weapon_flowers.png");
+        return images.get("img\\weapon_flowers.png");
     }
 
     public Image getTitleScreenBackground() {
-        return images.get("src\\main\\resources\\img\\titlescreen2.png");
+        return images.get("img\\titlescreen2.png");
     }
 
     public Image getBackgroundImage() {
-        return images.get("src\\main\\resources\\img\\level1.jpg");
+        return images.get("img\\level1.jpg");
     }
 
     public Image getDashboardPlayerContainerLeft() {
-        return images.get("src\\main\\resources\\img\\dashboard\\player_container_1.png");
+        return images.get("img\\dashboard\\player_container_1.png");
     }
 
     public Image getDashboardPlayerContainerRight() {
-        return images.get("src\\main\\resources\\img\\dashboard\\player_container_2.png");
+        return images.get("img\\dashboard\\player_container_2.png");
     }
 
     public Image getDashboardLivesContainer() {
-        return images.get("src\\main\\resources\\img\\dashboard\\lives_container.png");
+        return images.get("img\\dashboard\\lives_container.png");
     }
 
     public Image getDashboardlivesFull() {
-        return images.get("src\\main\\resources\\img\\dashboard\\lives_full.png");
+        return images.get("img\\dashboard\\lives_full.png");
     }
 
     public Image getDashboardlivesEmpty() {
-        return images.get("src\\main\\resources\\img\\dashboard\\lives_empty.png");
+        return images.get("img\\dashboard\\lives_empty.png");
     }
 
     public Image getLevelContainer() {
-        return images.get("src\\main\\resources\\img\\dashboard\\level_container.png");
+        return images.get("img\\dashboard\\level_container.png");
     }
 
     public Image getPlayerImageStill() {
-        return images.get("src\\main\\resources\\img\\player_still.png");
+        return images.get("img\\player_still.png");
     }
 
     public Image getOn() {
-        return images.get("src\\main\\resources\\img\\on.png");
+        return images.get("img\\on.png");
     }
 
     public Image getOff() {
-        return images.get("src\\main\\resources\\img\\off.png");
+        return images.get("img\\off.png");
     }
 
     public Image getSoundText() {
-        return images.get("src\\main\\resources\\img\\sound.png");
+        return images.get("img\\sound.png");
     }
 
     public Image getOptionsText() {
-        return images.get("src\\main\\resources\\img\\options.png");
+        return images.get("img\\options.png");
     }
 
     public Image getBackText() {
-        return images.get("src\\main\\resources\\img\\back.png");
+        return images.get("img\\back.png");
     }
 
     public Image getQuitText() {
-        return images.get("src\\main\\resources\\img\\quitText.png");
+        return images.get("img\\quitText.png");
     }
 
     public Image getPauseText() {
-        return images.get("src\\main\\resources\\img\\pausedText.png");
+        return images.get("img\\pausedText.png");
     }
 
     public Image getNewKeyText() {
-        return images.get("src\\main\\resources\\img\\newKeyText.png");
+        return images.get("img\\newKeyText.png");
     }
     
     public Image getShopBackGround() {
-        return images.get("src\\main\\resources\\img\\shopBackground.png");
+        return images.get("img\\shopBackground.png");
     }
 
     public Animation getPlayerWalkLeft() {
@@ -210,95 +210,95 @@ public class ResourcesWrapper {
     }
 
     public Image getBubbleImage1() {
-        return images.get("src\\main\\resources\\img\\yball1.png");
+        return images.get("img\\yball1.png");
     }
 
     public Image getBubbleImage2() {
-        return images.get("src\\main\\resources\\img\\yball2.png");
+        return images.get("img\\yball2.png");
     }
 
     public Image getBubbleImage3() {
-        return images.get("src\\main\\resources\\img\\yball3.png");
+        return images.get("img\\yball3.png");
     }
 
     public Image getBubbleImage4() {
-        return images.get("src\\main\\resources\\img\\rball4.png");
+        return images.get("img\\rball4.png");
     }
 
     public Image getBubbleImage5() {
-        return images.get("src\\main\\resources\\img\\rball5.png");
+        return images.get("img\\rball5.png");
     }
 
     public Image getBubbleImage6() {
-        return images.get("src\\main\\resources\\img\\rball6.png");
+        return images.get("img\\rball6.png");
     }
 
     public Image getPickupWeaponRegular() {
-        return images.get("src\\main\\resources\\img\\pickup_regular_weapon.png");
+        return images.get("img\\pickup_regular_weapon.png");
     }
 
     public Image getPickupWeaponDouble() {
-        return images.get("src\\main\\resources\\img\\pickup_weapon_double.png");
+        return images.get("img\\pickup_weapon_double.png");
     }
 
     public Image getPickupWeaponSticky() {
-        return images.get("src\\main\\resources\\img\\pickup_sticky.png");
+        return images.get("img\\pickup_sticky.png");
     }
 
     public Image getPickupWeaponFlowers() {
-        return images.get("src\\main\\resources\\img\\pickup_flowers.png");
+        return images.get("img\\pickup_flowers.png");
     }
 
     public Image getPickupPowerShield() {
-        return images.get("src\\main\\resources\\img\\pickup_shield.png");
+        return images.get("img\\pickup_shield.png");
     }
 
     public Image getPickupPowerInvincible() {
-        return images.get("src\\main\\resources\\img\\pickup_invincible.png");
+        return images.get("img\\pickup_invincible.png");
     }
 
     public Image getPickupPowerMoney() {
-        return images.get("src\\main\\resources\\img\\pickup_money.png");
+        return images.get("img\\pickup_money.png");
     }
 
     public Image getPickupPowerSpeedup() {
-        return images.get("src\\main\\resources\\img\\pickup_speed.png");
+        return images.get("img\\pickup_speed.png");
     }
 
     public Image getPickupPowerPoints() {
-        return images.get("src\\main\\resources\\img\\pickup_points.png");
+        return images.get("img\\pickup_points.png");
     }
 
     public Image getPickupUtilitySplit() {
-        return images.get("src\\main\\resources\\img\\pickup_split.png");
+        return images.get("img\\pickup_split.png");
     }
 
     public Image getPickupUtilityFreeze() {
-        return images.get("src\\main\\resources\\img\\pickup_freeze.png");
+        return images.get("img\\pickup_freeze.png");
     }
 
     public Image getPickupUtilitySlow() {
-        return images.get("src\\main\\resources\\img\\pickup_slow_down.png");
+        return images.get("img\\pickup_slow_down.png");
     }
 
     public Image getPickupUtilityLevelwon() {
-        return images.get("src\\main\\resources\\img\\pickup_level_won.png");
+        return images.get("img\\pickup_level_won.png");
     }
 
     public Image getPickupUtilityTime() {
-        return images.get("src\\main\\resources\\img\\pickup_time.png");
+        return images.get("img\\pickup_time.png");
     }
 
     public Image getPickupUtilityLife() {
-        return images.get("src\\main\\resources\\img\\pickup_life.png");
+        return images.get("img\\pickup_life.png");
     }
 
     public Image getPowerShield() {
-        return images.get("src\\main\\resources\\img\\powerup_shield.png");
+        return images.get("img\\powerup_shield.png");
     }
 
     public Image getPowerInvincible() {
-        return images.get("src\\main\\resources\\img\\powerup_invincible.png");
+        return images.get("img\\powerup_invincible.png");
     }
 
     public Sound getBubblePop() {
@@ -322,43 +322,43 @@ public class ResourcesWrapper {
     }
 
     public Image getContinueText() {
-        return images.get("src\\main\\resources\\img\\continue.png");
+        return images.get("img\\continue.png");
     }
 
     public Image getLoggerText() {
-        return images.get("src\\main\\resources\\img\\logger.png");
+        return images.get("img\\logger.png");
     }
 
     public Image getKeyText() {
-        return images.get("src\\main\\resources\\img\\keyBindings.png");
+        return images.get("img\\keyBindings.png");
     }
 
     public Image getShopText() {
-        return images.get("src\\main\\resources\\img\\shop.png");
+        return images.get("img\\shop.png");
     }
     
     public Image getSpecialWeapon() {
-        return images.get("src\\main\\resources\\img\\pickup_weapon_special.png");
+        return images.get("img\\pickup_weapon_special.png");
     }
     
     public Image getBuy() {
-        return images.get("src\\main\\resources\\img\\buy.png");
+        return images.get("img\\buy.png");
     }
 
     public Image getPlayer2On() {
-        return images.get("src\\main\\resources\\img\\player2TextOn.png");
+        return images.get("img\\player2TextOn.png");
     }
 
     public Image getPlayer1Off() {
-        return images.get("src\\main\\resources\\img\\player1TextOff.png");
+        return images.get("img\\player1TextOff.png");
     }
     
     public Image getPlayer1On() {
-        return images.get("src\\main\\resources\\img\\player1TextOn.png");
+        return images.get("img\\player1TextOn.png");
     }
 
     public Image getPlayer2Off() {
-        return images.get("src\\main\\resources\\img\\player2TextOff.png");
+        return images.get("img\\player2TextOff.png");
     }
     
     public TrueTypeFont getCountdownFont() {
