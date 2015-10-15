d8e8dfcdda8b359620470b123cca42a73603de35
Wouter Smit <pathemeous@gmail.com>
2015-10-15 21:25:05 +0200

    Refactor some to Controller
    < organistaion

--- src/main/java/nl/tudelft/semgroup4/StartScreenStateController.java
+++ src/main/java/nl/tudelft/semgroup4/StartScreenStateController.java
@@ -1,6 +1,19 @@
 package nl.tudelft.semgroup4;
 
+import java.awt.Font;
+
 import nl.tudelft.semgroup4.resources.ResourcesWrapper;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
+import org.newdawn.slick.gui.MouseOverArea;
 
 /**
  * Controller that performs logic for the StartScreenState.
@@ -13,6 +26,9 @@ public class StartScreenStateController {
     private StartScreenState state;
     private ResourcesWrapper resources;
 
+    private final String highScoreText = "HIGHSCORES";
+    private TrueTypeFont typeFont;
+
     /**
      * Creates a new controler for a specific {@link StartScreenState}.
      * 
@@ -24,6 +40,42 @@ public class StartScreenStateController {
     public StartScreenStateController(StartScreenState state, ResourcesWrapper resources) {
         this.state = state;
         this.resources = resources;
+    }
+
+    /**
+     * Creates the HighScore button with the appropriate text.
+     * 
+     * @throws SlickException
+     *             If creating the {@link Image} goes wrong.
+     */
+    protected MouseOverArea createHighscoresButton(GameContainer container)
+            throws SlickException {
+        Shape shape =
+                new Rectangle(650, 650, typeFont.getWidth(highScoreText),
+                        typeFont.getHeight());
+        shape = shape.transform(Transform.createRotateTransform(6, 650, 650));
+        Font font = new Font("Verdana", Font.BOLD, 36);
+        typeFont = resources.createFont(font, true);
+        Image highScoreButton =
+                new Image(typeFont.getWidth(highScoreText), typeFont.getHeight());
+
+        shape =
+                new Rectangle(650, 650, typeFont.getWidth(highScoreText),
+                        typeFont.getHeight());
+        shape = shape.transform(Transform.createRotateTransform(6, 650, 650));
+
+        return new MouseOverArea(container, highScoreButton, shape);
+    }
+
+    /**
+     * Draws the text of the HighsScoresButton on the screen.
+     * 
+     * @param graphics
+     *            {@link Graphics} - The graphics that this method may use to draw.
+     */
+    protected void drawHighScoresButton(Graphics graphics) {
+        graphics.rotate(650, 650, 340);
+        typeFont.drawString(650, 650, highScoreText, Color.red);
     }
 
 }
