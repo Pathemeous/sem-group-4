2c21203d793e1b4d66eec2db84265f8d41a89c65
Casper Athmer <casperathmer92@gmail.com>
2015-10-15 15:46:09 +0200

    Used system specific file separator for all the keys

--- src/main/java/nl/tudelft/semgroup4/resources/ResourcesWrapper.java
+++ src/main/java/nl/tudelft/semgroup4/resources/ResourcesWrapper.java
@@ -23,6 +23,7 @@ public class ResourcesWrapper {
     private final HashMap<String, Sound> sounds;
     private final HashMap<String, Animation> animations;
     private final HashMap<String, Music> music;
+    private final String fileSeparator;
     
     /**
      * Creates a new resourceswrapper, which contains all the resources
@@ -33,6 +34,7 @@ public class ResourcesWrapper {
         sounds = Resources.getSounds();
         animations = Resources.getAnimations();
         music = Resources.getMusic();
+        fileSeparator = System.getProperty("file.separator");
     }
     
 
@@ -101,262 +103,266 @@ public class ResourcesWrapper {
     }
 
     public Image getWallImage() {
-        return images.get("img\\wall2_h.png");
+        return images.get("img" + fileSeparator + "wall2_h.png");
     }
 
     public Image getVwallImage() {
-        return images.get("img\\wall2_v.png");
+        return images.get("img" + fileSeparator + "wall2_v.png");
     }
     
     public Image getSmallHWallImage() {
-        return images.get("img\\small_wall_h.png");
+        return images.get("img" + fileSeparator + "small_wall_h.png");
     }
     
     public Image getSmallVWallImage() {
-        return images.get("img\\small_wall_v.png");
+        return images.get("img" + fileSeparator + "small_wall_v.png");
     }
 
     public Image getWeaponImageRegular() {
-        return images.get("img\\weapon_arrow.png");
+        return images.get("img" + fileSeparator + "weapon_arrow.png");
     }
 
     public Image getWeaponImageSticky() {
-        return images.get("img\\weapon_arrow.png");
+        return images.get("img" + fileSeparator + "weapon_arrow.png");
     }
 
     public Image getWeaponImageFlower() {
-        return images.get("img\\weapon_flowers.png");
+        return images.get("img" + fileSeparator + "weapon_flowers.png");
     }
 
     public Image getTitleScreenBackground() {
-        return images.get("img\\titlescreen2.png");
+        return images.get("img" + fileSeparator + "titlescreen2.png");
     }
 
     public Image getBackgroundImage() {
-        return images.get("img\\level1.jpg");
+        return images.get("img" + fileSeparator + "level1.jpg");
     }
 
     public Image getDashboardPlayerContainerLeft() {
-        return images.get("img\\dashboard\\player_container_1.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator 
+                + "player_container_1.png");
     }
 
     public Image getDashboardPlayerContainerRight() {
-        return images.get("img\\dashboard\\player_container_2.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator 
+                + "player_container_2.png");
     }
 
     public Image getDashboardLivesContainer() {
-        return images.get("img\\dashboard\\lives_container.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator 
+                + "lives_container.png");
     }
 
     public Image getDashboardlivesFull() {
-        return images.get("img\\dashboard\\lives_full.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator + "lives_full.png");
     }
 
     public Image getDashboardlivesEmpty() {
-        return images.get("img\\dashboard\\lives_empty.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator + "lives_empty.png");
     }
 
     public Image getLevelContainer() {
-        return images.get("img\\dashboard\\level_container.png");
+        return images.get("img" + fileSeparator + "dashboard" + fileSeparator 
+                + "level_container.png");
     }
 
     public Image getPlayerImageStill() {
-        return images.get("img\\player_still.png");
+        return images.get("img" + fileSeparator + "player_still.png");
     }
 
     public Image getOn() {
-        return images.get("img\\on.png");
+        return images.get("img" + fileSeparator + "on.png");
     }
 
     public Image getOff() {
-        return images.get("img\\off.png");
+        return images.get("img" + fileSeparator + "off.png");
     }
 
     public Image getSoundText() {
-        return images.get("img\\sound.png");
+        return images.get("img" + fileSeparator + "sound.png");
     }
 
     public Image getOptionsText() {
-        return images.get("img\\options.png");
+        return images.get("img" + fileSeparator + "options.png");
     }
 
     public Image getBackText() {
-        return images.get("img\\back.png");
+        return images.get("img" + fileSeparator + "back.png");
     }
 
     public Image getQuitText() {
-        return images.get("img\\quitText.png");
+        return images.get("img" + fileSeparator + "quitText.png");
     }
 
     public Image getPauseText() {
-        return images.get("img\\pausedText.png");
+        return images.get("img" + fileSeparator + "pausedText.png");
     }
 
     public Image getNewKeyText() {
-        return images.get("img\\newKeyText.png");
+        return images.get("img" + fileSeparator + "newKeyText.png");
     }
     
     public Image getShopBackGround() {
-        return images.get("img\\shopBackground.png");
+        return images.get("img" + fileSeparator + "shopBackground.png");
     }
 
     public Animation getPlayerWalkLeft() {
-        return animations.get("animation\\player\\pl");
+        return animations.get("animation" + fileSeparator + "player" + fileSeparator + "pl");
     }
 
     public Animation getPlayerWalkRight() {
-        return animations.get("animation\\player\\pr");
+        return animations.get("animation" + fileSeparator + "player" + fileSeparator + "pr");
     }
 
     public Image getBubbleImage1() {
-        return images.get("img\\yball1.png");
+        return images.get("img" + fileSeparator + "yball1.png");
     }
 
     public Image getBubbleImage2() {
-        return images.get("img\\yball2.png");
+        return images.get("img" + fileSeparator + "yball2.png");
     }
 
     public Image getBubbleImage3() {
-        return images.get("img\\yball3.png");
+        return images.get("img" + fileSeparator + "yball3.png");
     }
 
     public Image getBubbleImage4() {
-        return images.get("img\\rball4.png");
+        return images.get("img" + fileSeparator + "rball4.png");
     }
 
     public Image getBubbleImage5() {
-        return images.get("img\\rball5.png");
+        return images.get("img" + fileSeparator + "rball5.png");
     }
 
     public Image getBubbleImage6() {
-        return images.get("img\\rball6.png");
+        return images.get("img" + fileSeparator + "rball6.png");
     }
 
     public Image getPickupWeaponRegular() {
-        return images.get("img\\pickup_regular_weapon.png");
+        return images.get("img" + fileSeparator + "pickup_regular_weapon.png");
     }
 
     public Image getPickupWeaponDouble() {
-        return images.get("img\\pickup_weapon_double.png");
+        return images.get("img" + fileSeparator + "pickup_weapon_double.png");
     }
 
     public Image getPickupWeaponSticky() {
-        return images.get("img\\pickup_sticky.png");
+        return images.get("img" + fileSeparator + "pickup_sticky.png");
     }
 
     public Image getPickupWeaponFlowers() {
-        return images.get("img\\pickup_flowers.png");
+        return images.get("img" + fileSeparator + "pickup_flowers.png");
     }
 
     public Image getPickupPowerShield() {
-        return images.get("img\\pickup_shield.png");
+        return images.get("img" + fileSeparator + "pickup_shield.png");
     }
 
     public Image getPickupPowerInvincible() {
-        return images.get("img\\pickup_invincible.png");
+        return images.get("img" + fileSeparator + "pickup_invincible.png");
     }
 
     public Image getPickupPowerMoney() {
-        return images.get("img\\pickup_money.png");
+        return images.get("img" + fileSeparator + "pickup_money.png");
     }
 
     public Image getPickupPowerSpeedup() {
-        return images.get("img\\pickup_speed.png");
+        return images.get("img" + fileSeparator + "pickup_speed.png");
     }
 
     public Image getPickupPowerPoints() {
-        return images.get("img\\pickup_points.png");
+        return images.get("img" + fileSeparator + "pickup_points.png");
     }
 
     public Image getPickupUtilitySplit() {
-        return images.get("img\\pickup_split.png");
+        return images.get("img" + fileSeparator + "pickup_split.png");
     }
 
     public Image getPickupUtilityFreeze() {
-        return images.get("img\\pickup_freeze.png");
+        return images.get("img" + fileSeparator + "pickup_freeze.png");
     }
 
     public Image getPickupUtilitySlow() {
-        return images.get("img\\pickup_slow_down.png");
+        return images.get("img" + fileSeparator + "pickup_slow_down.png");
     }
 
     public Image getPickupUtilityLevelwon() {
-        return images.get("img\\pickup_level_won.png");
+        return images.get("img" + fileSeparator + "pickup_level_won.png");
     }
 
     public Image getPickupUtilityTime() {
-        return images.get("img\\pickup_time.png");
+        return images.get("img" + fileSeparator + "pickup_time.png");
     }
 
     public Image getPickupUtilityLife() {
-        return images.get("img\\pickup_life.png");
+        return images.get("img" + fileSeparator + "pickup_life.png");
     }
 
     public Image getPowerShield() {
-        return images.get("img\\powerup_shield.png");
+        return images.get("img" + fileSeparator + "powerup_shield.png");
     }
 
     public Image getPowerInvincible() {
-        return images.get("img\\powerup_invincible.png");
+        return images.get("img" + fileSeparator + "powerup_invincible.png");
     }
 
     public Sound getBubblePop() {
-        return sounds.get("sound\\pop.ogg");
+        return sounds.get("sound" + fileSeparator + "pop.ogg");
     }
 
     public Sound getWeaponFire() {
-        return sounds.get("sound\\weaponFire.ogg");
+        return sounds.get("sound" + fileSeparator + "weaponFire.ogg");
     }
 
     public Sound getDeath() {
-        return sounds.get("sound\\death.ogg");
+        return sounds.get("sound" + fileSeparator + "death.ogg");
     }
 
     public Sound getTimeUp() {
-        return sounds.get("sound\\timeUp.ogg");
+        return sounds.get("sound" + fileSeparator + "timeUp.ogg");
     }
 
     public Music getTitleScreenMusic() {
-        return music.get("music\\titleScreen.ogg");
+        return music.get("music" + fileSeparator + "titleScreen.ogg");
     }
 
     public Image getContinueText() {
-        return images.get("img\\continue.png");
+        return images.get("img" + fileSeparator + "continue.png");
     }
 
     public Image getLoggerText() {
-        return images.get("img\\logger.png");
+        return images.get("img" + fileSeparator + "logger.png");
     }
 
     public Image getKeyText() {
-        return images.get("img\\keyBindings.png");
+        return images.get("img" + fileSeparator + "keyBindings.png");
     }
 
     public Image getShopText() {
-        return images.get("img\\shop.png");
+        return images.get("img" + fileSeparator + "shop.png");
     }
     
     public Image getSpecialWeapon() {
-        return images.get("img\\pickup_weapon_special.png");
+        return images.get("img" + fileSeparator + "pickup_weapon_special.png");
     }
     
     public Image getBuy() {
-        return images.get("img\\buy.png");
+        return images.get("img" + fileSeparator + "buy.png");
     }
 
     public Image getPlayer2On() {
-        return images.get("img\\player2TextOn.png");
+        return images.get("img" + fileSeparator + "player2TextOn.png");
     }
 
     public Image getPlayer1Off() {
-        return images.get("img\\player1TextOff.png");
+        return images.get("img" + fileSeparator + "player1TextOff.png");
     }
     
     public Image getPlayer1On() {
-        return images.get("img\\player1TextOn.png");
+        return images.get("img" + fileSeparator + "player1TextOn.png");
     }
 
     public Image getPlayer2Off() {
-        return images.get("img\\player2TextOff.png");
+        return images.get("img" + fileSeparator + "player2TextOff.png");
     }
 }
