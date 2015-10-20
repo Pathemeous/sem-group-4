7d3fff7ee6cd27806d5be8974ecf5970b0374387
Wouter Smit <pathemeous@gmail.com>
2015-10-20 14:24:17 +0200

    Add tests for MouseOverAreas
    < testing

--- src/test/java/nl/tudelft/semgroup4/StartScreenStateControllerTest.java
+++ src/test/java/nl/tudelft/semgroup4/StartScreenStateControllerTest.java
@@ -3,6 +3,7 @@ package nl.tudelft.semgroup4;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import nl.tudelft.semgroup4.resources.ResourcesWrapper;
@@ -10,24 +11,92 @@ import nl.tudelft.semgroup4.resources.ResourcesWrapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.gui.MouseOverArea;
 
 public class StartScreenStateControllerTest {
 
     private ResourcesWrapper mockedResources;
     private StartScreenStateController defaultInstance;
+    private TrueTypeFont mockedFont;
 
     @Before
     public void setUp() throws Exception {
         mockedResources = Mockito.mock(ResourcesWrapper.class);
         defaultInstance = new StartScreenStateController(mockedResources);
+        mockedFont = Mockito.mock(TrueTypeFont.class);
+        defaultInstance.setFont(mockedFont);
     }
 
     @Test
     public void testStartScreenStateController() {
         assertNotNull(defaultInstance);
         assertEquals(mockedResources, defaultInstance.getResources());
+    }
+
+    @Test
+    public void testCreatePlayer1Button() {
+        Input mockedInput = Mockito.mock(Input.class);
+        GameContainer mockedContainer = Mockito.mock(GameContainer.class);
+        Mockito.when(mockedContainer.getInput()).thenReturn(mockedInput);
+        MouseOverArea result = null;
+        assertNull(result);
+        result = defaultInstance.createPlayer1Button(mockedContainer);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCreatePlayer2Button() {
+        Input mockedInput = Mockito.mock(Input.class);
+        GameContainer mockedContainer = Mockito.mock(GameContainer.class);
+        Mockito.when(mockedContainer.getInput()).thenReturn(mockedInput);
+        MouseOverArea result = null;
+        assertNull(result);
+        result = defaultInstance.createPlayer2Button(mockedContainer);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCreateOptionsButton() {
+        Input mockedInput = Mockito.mock(Input.class);
+        GameContainer mockedContainer = Mockito.mock(GameContainer.class);
+        Mockito.when(mockedContainer.getInput()).thenReturn(mockedInput);
+        MouseOverArea result = null;
+        assertNull(result);
+        result = defaultInstance.createOptionsButton(mockedContainer);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCreateQuitButton() {
+        Input mockedInput = Mockito.mock(Input.class);
+        GameContainer mockedContainer = Mockito.mock(GameContainer.class);
+        Mockito.when(mockedContainer.getInput()).thenReturn(mockedInput);
+        MouseOverArea result = null;
+        assertNull(result);
+        result = defaultInstance.createQuitButton(mockedContainer);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCreateHighscoresButton() throws SlickException {
+        Image mockedImage = Mockito.mock(Image.class);
+        
+        Mockito.when(mockedFont.getWidth(Mockito.any())).thenReturn(5);
+        Mockito.when(mockedFont.getHeight()).thenReturn(5);
+        Mockito.when(mockedResources.createImage(Mockito.anyInt(), Mockito.anyInt())).thenReturn(mockedImage);
+        
+        Input mockedInput = Mockito.mock(Input.class);
+        GameContainer mockedContainer = Mockito.mock(GameContainer.class);
+        Mockito.when(mockedContainer.getInput()).thenReturn(mockedInput);
+        MouseOverArea result = null;
+        assertNull(result);
+        result = defaultInstance.createHighscoresButton(mockedContainer);
+        assertNotNull(result);
     }
 
     @Test
