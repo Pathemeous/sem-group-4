455b2a323a23a7f70bd797f8449554c061e39079
Wouter Smit <pathemeous@gmail.com>
2015-10-30 15:23:54 +0100

    Clean up JavaDoc for AbstractGameObject
    Some JavaDocs could be deleted (they are specified in the override).
    
    Updated the class JavaDoc to reflect its implementation of the new
    GameObject interface.
    
    Kept all comments that further specified the GameObject interface.
    
    < documentation

--- src/main/java/nl/tudelft/model/AbstractGameObject.java
+++ src/main/java/nl/tudelft/model/AbstractGameObject.java
@@ -1,8 +1,6 @@
 package nl.tudelft.model;
 
 import nl.tudelft.controller.Modifiable;
-import nl.tudelft.controller.Renderable;
-import nl.tudelft.controller.Updateable;
 import nl.tudelft.controller.util.SemRectangle;
 
 import org.newdawn.slick.GameContainer;
@@ -12,8 +10,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Shape;
 
 /**
- * This class represents an {@link Updateable} (dynamic) and {@link Renderable} (visible and
- * collidable) object within the {@link Game} environment.
+ * This class represents a framework implementation of the {@link GameObject} interface.
  * 
  * <p>
  * This class provides common functionality for this kind of objects, including positioning and
@@ -43,72 +40,39 @@ public abstract class AbstractGameObject implements GameObject {
     }
 
     @Override
+    public abstract <T extends Modifiable> void update(T container, int delta)
+            throws SlickException;
+
+    @Override
     public void render(GameContainer container, Graphics graphics) throws SlickException {
         graphics.drawImage(getImage(), locX, locY);
     }
 
     @Override
-    public abstract <T extends Modifiable> void update(T container, int delta)
-            throws SlickException;
-
-    /**
-     * Accesses the image field.
-     * 
-     * @return image {@link Image} - The image that this object is rendered with.
-     */
-    @Override
     public Image getImage() {
         return image;
     }
 
-    /**
-     * Sets a new image for this object.
-     * 
-     * @param image
-     *            {@link Image} - The new image to render.
-     */
     @Override
     public void setImage(Image image) {
         this.image = image;
     }
 
-    /**
-     * Sets a new x-coordinate for this object.
-     * 
-     * @param locX
-     *            float - The x-coordinate for the top-left corner of this object.
-     */
     @Override
     public void setLocX(float locX) {
         this.locX = locX;
     }
 
-    /**
-     * Accesses the x-coordinate of this object.
-     * 
-     * @return float - The x-coordinate.
-     */
     @Override
     public float getLocX() {
         return locX;
     }
 
-    /**
-     * Accesses the y-coordinate of this object.
-     * 
-     * @return float - The y-coordinate.
-     */
     @Override
     public float getLocY() {
         return locY;
     }
 
-    /**
-     * Sets a new y-coordinate for this object.
-     * 
-     * @param locY
-     *            float - the y-coordinate for the top-left corner of this object.
-     */
     @Override
     public void setLocY(float locY) {
         this.locY = locY;
