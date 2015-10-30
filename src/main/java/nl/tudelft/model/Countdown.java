b30e89546ddc8749ec709b631cdf8403dcd5d36c
Wouter Smit <pathemeous@gmail.com>
2015-10-30 15:35:56 +0100

    Add comments to Countdown
    < documentation

--- src/main/java/nl/tudelft/model/Countdown.java
+++ src/main/java/nl/tudelft/model/Countdown.java
@@ -9,23 +9,29 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 
+/**
+ * This class represents the countdown that happens at the start of each level.
+ */
 public class Countdown implements Renderable {
-    
+
     private final Game game;
     private int countdownTimer = 180;
     private final TrueTypeFont typeFont;
-    
+
     /**
-     * Constructor of this class. Creates a countdown object.
-     * @param game : the game that this countdown belongs to.
-     * @param resources : resources wrapper.
+     * Constructs a new instance of {@link Countdown}.
+     * 
+     * @param game
+     *            {@link Game} - The game that this countdown belongs to.
+     * @param resources
+     *            {@link ResourcesWrapper} - The resourceswrapper used in this instance.
      */
     public Countdown(Game game, ResourcesWrapper resources) {
         this.game = game;
         game.setPaused(true);
         typeFont = resources.getCountdownFont();
     }
-    
+
     /**
      * Resets the countdown, so that it starts over.
      */
@@ -33,7 +39,10 @@ public class Countdown implements Renderable {
         game.setPaused(true);
         countdownTimer = 180;
     }
-    
+
+    /**
+     * Renders the rounded integer value of the countdown on the screen.
+     */
     @Override
     public void render(GameContainer container, Graphics graphics) throws SlickException {
         if (countdownTimer > 0) {
@@ -41,7 +50,7 @@ public class Countdown implements Renderable {
             typeFont.drawString(container.getWidth() / 2 - 25, 100.0f, count, Color.black);
         }
     }
-    
+
     /**
      * Updates the countdown. As long as the counter is higher than 0, one is subtracted from it.
      * When the counter equals 0, the game is unpaused.
@@ -52,13 +61,24 @@ public class Countdown implements Renderable {
         } else if (countdownTimer == 0) {
             game.setPaused(false);
             countdownTimer--;
-        } 
+        }
     }
-    
+
+    /**
+     * Returns the current countdown value.
+     * 
+     * @return int - The amount of clocks until the countdown is finished.
+     */
     protected int getCounter() {
         return countdownTimer;
     }
-    
+
+    /**
+     * Sets the countdown value to the specified value.
+     * 
+     * @param counter
+     *            int - The amount of clocks to count until the countdown is finished.
+     */
     protected void setCounter(int counter) {
         countdownTimer = counter;
     }
